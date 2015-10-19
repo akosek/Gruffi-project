715bd31364e970d6ebec0ea471566d076b6b453a
Agata Kosek <agata@Agatas-MacBook-Pro.local>
2015-10-20 01:33:03 +0200

    final changes

--- guffi.js
+++ guffi.js
@@ -1,19 +1,11 @@
-
-/*var button1 = document.getElementById('bregister');
-var rForm = document.getElementById("rForm");
-
-function toggle() {
-		rForm.style.visibility = rForm.style.visibility === "hidden" ? "visible" :  "hidden";
-}
-
-toggle();
-
-button1.addEventListener("click", toggle, false);
-
-*/
-
 $(document).ready(function(){
     $("#bregister").click(function(){
         $("#la").toggle();
+    });
+});
+
+$(document).ready(function(){
+    $("#blogin").click(function(){
+        $("#lal").toggle();
     });
 });
